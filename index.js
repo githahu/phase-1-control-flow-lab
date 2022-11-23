666f7215b9fa227c78293c56e11c5ed56599d25b
Edwin <edwin.njoroge@student.moringaschool.com>
2022-11-23 07:19:40 +0300

    commit tree 439f103a70fd6ed1e59c1ea60359d812bbbf8132

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet) {
+  
+  if(feet <= 400){
+    return "This one is on me"
+  
+  }
+  if(feet >= 2000 && <= 2500){
+    return "I will gladly take your thirty bucks"
+  } else if(feet >=2501){
+    return "No can do."
+  }
+ 
+
 
 function ternaryCheckCity(){
   // Write your code here!
