8695cb70411f634ab10e82d5feb2d26871b79ffa
Edwin <edwin.njoroge@student.moringaschool.com>
2022-11-23 19:08:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if(feet <= 400){
+    return "This one is on me!"
+  } 
+  if(feet>= 2000 && feet <=2500){
+    return "I will gladly take your thirty bucks."
+  }else if (feet >=2501){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(City){
+  return(City==="NYC" ? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Tip){
+  switch(Tip){
+    case "generous":
+      return "Thank you so much."  
+      case "not as generous":
+        return "Thank you."
+      default:
+        return "Bye."
+  }
 }
